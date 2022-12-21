66bd91f1bfe1151b62cd77907420fb51f4ef6e50
Alex Valentiner <45672183+alex123a@users.noreply.github.com>
2022-12-21 18:44:06 +0100

    Unit tests done

--- jhotdraw-samples/jhotdraw-samples-misc/src/test/java/org/jhotdraw/samples/svg/figures/SVGTextAreaFigureTest.java
+++ jhotdraw-samples/jhotdraw-samples-misc/src/test/java/org/jhotdraw/samples/svg/figures/SVGTextAreaFigureTest.java
@@ -4,6 +4,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
 import static org.junit.Assert.*;
 
 public class SVGTextAreaFigureTest {
@@ -12,7 +15,7 @@ public class SVGTextAreaFigureTest {
 
     @Before
     public void setUp() throws Exception {
-        textArea = new SVGTextAreaFigure("start");
+        textArea = new SVGTextAreaFigure("Start text");
     }
 
     @After
@@ -21,17 +24,40 @@ public class SVGTextAreaFigureTest {
     }
 
     @Test
-    public void lineCompletion() {
+    public void  createTabStops() {
+        // This test is made to test if the right amount of tab stops are made
+        float tabWidth = 3;
+        Rectangle2D.Double textRect = new Rectangle2D.Double(3, 3, 12, 4);
+        float[] tabStops = textArea.createTabStops(tabWidth, textRect);
+        // The right length is calculated by rectangle width divided by tab width
+        int expected = (int) (textRect.width / tabWidth);
+        assertEquals(expected, tabStops.length);
+    }
+
+    @Test
+    public void setBounds() {
+        Rectangle2D.Double beforeNewBounds = textArea.getBounds();
+        Point2D.Double anchor = new Point2D.Double(3.43, 4.43);
+        Point2D.Double lead = new Point2D.Double(5.43, 6.43);
+        textArea.setBounds(anchor, lead);
+        assertNotEquals(beforeNewBounds, textArea.getBounds());
     }
 
     @Test
     public void getText() {
-        assertEquals("start", textArea.getText());
+        assertEquals("Start text", textArea.getText());
     }
 
     @Test
     public void setText() {
         textArea.setText("new text");
         assertEquals("new text", textArea.getText());
+    }
+
+    @Test
+    public void isEmpty() {
+        assertFalse(textArea.isEmpty());
+        textArea.setText(null);
+        assertTrue(textArea.isEmpty());
     }
 }
