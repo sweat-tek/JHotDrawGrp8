86addc32e42105052c2cf0930428dc8b64e2eec8
Mikkel <mikkelpriisholm@gmail.com>
2023-01-03 15:06:34 +0100

    Update SVGGroupFigureTest.java

--- jhotdraw-samples/jhotdraw-samples-misc/src/test/java/org/jhotdraw/samples/svg/figures/SVGGroupFigureTest.java
+++ jhotdraw-samples/jhotdraw-samples-misc/src/test/java/org/jhotdraw/samples/svg/figures/SVGGroupFigureTest.java
@@ -1,17 +1,13 @@
 package org.jhotdraw.samples.svg.figures;
 
-import org.jhotdraw.draw.AttributeKey;
-import org.jhotdraw.samples.svg.action.FigureUndoAction;
+import org.jhotdraw.draw.handle.BoundsOutlineHandle;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
-import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
-import java.util.LinkedList;
 
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertTrue;
@@ -21,7 +17,7 @@ public class SVGGroupFigureTest {
 
     private SVGGroupFigure svgGroupFigure;
 
-    Rectangle2D.Double groupBounds;
+    Rectangle2D.Double initialGroupBounds;
 
     @Mock
     private Graphics2D graphics2D;
@@ -30,33 +26,30 @@ public class SVGGroupFigureTest {
     public void beforeClass() throws Exception {
         graphics2D = mock(Graphics2D.class);
         svgGroupFigure = new SVGGroupFigure();
-        groupBounds = svgGroupFigure.getBounds();
+        initialGroupBounds = svgGroupFigure.getBounds();
     }
 
     @Test
     public void hasBounds() {
-        assertNotNull(groupBounds);
+        assertNotNull(initialGroupBounds);
     }
 
     @Test
-    public void transform() {
-        AffineTransform affineTransform = new AffineTransform();
-        affineTransform.setToTranslation(5d, 5d);
-        svgGroupFigure.transform(affineTransform);
-
-        assertFalse(svgGroupFigure.getBounds().x == groupBounds.x);
-        assertFalse(svgGroupFigure.getBounds().y == groupBounds.y);
+    public void createHandles() {
+        assertTrue(svgGroupFigure.createHandles(0).getFirst() instanceof BoundsOutlineHandle);
+        assertTrue(svgGroupFigure.createHandles(0).getLast() instanceof LinkHandle);
     }
 
     @Test
-    public void getActions() {
-        // svgGroupFigure must be provided a value for TRANSFORM in order to add FigureUndoAction
-        // this is because it cannot be mocked, as it is part of the code unit being tested
-        svgGroupFigure.set(new AttributeKey<>("transform", AffineTransform.class), new AffineTransform());
-        LinkedList<Action> actionArrayList = (LinkedList<Action>) svgGroupFigure.getActions(null);
+    public void createGroup() {
+        assertTrue(svgGroupFigure.isEmpty());
+        assertTrue(svgGroupFigure.getBounds().width == 0);
 
-        assertTrue(actionArrayList.isEmpty() == false);
-        assertTrue(actionArrayList.get(0).getClass() == FigureUndoAction.class);
+        svgGroupFigure.add(new SVGRectFigure(10d, 10d, 8d, 4d));
+        svgGroupFigure.add(new SVGRectFigure(20d, 20d, 8d, 4d));
+
+        assertFalse(svgGroupFigure.isEmpty());
+        assertTrue(svgGroupFigure.getBounds().width > 0);
     }
 
     @After
